ac302ffda0899089c18d828ceb1d2e13ab018e2f
Basudev Haldar <dev628140@gmail.com>
2024-09-09 02:48:05 +0530

    first

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import App from "./components/App";
+
+// Create a root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render your app using the root
+root.render(<App />);
